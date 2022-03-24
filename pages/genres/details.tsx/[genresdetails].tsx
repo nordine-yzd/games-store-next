e99e70738b5d4703c73db4d7d47c6ca26b4db8f8
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-03-24 15:20:40 +0100

    essaie de rajouter les details par games

--- pages/genres/details.tsx/[genresdetails].tsx
+++ pages/genres/details.tsx/[genresdetails].tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from "next";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
-import { Layout } from "../../components/layout";
+import { Layout } from "../../../components/layout";
 import React from "react";
 
 type AllGenresTyped = {
